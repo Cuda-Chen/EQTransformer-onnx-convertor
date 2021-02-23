59b4f7b9b03ddc8c8e818e08fd9501a40752046d
Cuda-Chen <clh960524@gmail.com>
2021-02-23 16:41:49 +0800

    add station list for mseed_predictor

--- compare.py
+++ compare.py
@@ -1,3 +1,18 @@
-from EQTransformer.core.mseed_predictor import mseed_predictor, _mseed2nparry
+from EQTransformer.core.EqT_utils import f1, SeqSelfAttention, FeedForward, LayerNormalization
+from EQTransformer.core.mseed_predictor import mseed_predictor, _mseed2nparry, PreLoadGeneratorTest
+import keras
+from keras.models import load_model
+import keras2onnx
 
-
+# original
+# detection.ipynb
+model = load_model('EqT_model.h5',
+                    custom_objects={
+                       'SeqSelfAttention': SeqSelfAttention, 
+                       'FeedForward': FeedForward,
+                       'LayerNormalization': LayerNormalization, 
+                   'f1': f1})
+model.compile(loss = ['binary_crossentropy', 'binary_crossentropy', 'binary_crossentropy'],
+              loss_weights = [0.02, 0.40, 0.58],           
+              optimizer = Adam(lr = 0.001),
+              metrics = [f1])
