bcfee78fdfa0d4ab483660972bd2e3e12847c2bb
Cuda-Chen <clh960524@gmail.com>
2021-02-25 15:33:01 +0800

    seperate Keras and ONNX port for result analysis

--- compare.py
+++ compare.py
@@ -13,9 +13,17 @@ import numpy as np
 
 import onnxruntime
 
+params_pred = {'batch_size': 500,
+               'norm_mode': 'std'}
+
+args = {'input_dir': 'downloads_mseeds',
+        'stations_json': 'station_list.json',
+        'overlap': 0.3}
+
 # original
 # detection.ipynb
-"""model = load_model('EqT_model.h5',
+print('Keras:')
+model = load_model('EqT_model.h5',
                     custom_objects={
                        'SeqSelfAttention': SeqSelfAttention, 
                        'FeedForward': FeedForward,
@@ -24,17 +32,7 @@ import onnxruntime
 model.compile(loss = ['binary_crossentropy', 'binary_crossentropy', 'binary_crossentropy'],
               loss_weights = [0.02, 0.40, 0.58],           
               optimizer = Adam(lr = 0.001),
-              metrics = [f1])"""
-
-sess_options = onnxruntime.SessionOptions()
-sess = onnxruntime.InferenceSession('eqt_model.onnx', sess_options)
-
-params_pred = {'batch_size': 500,
-               'norm_mode': 'std'}
-
-args = {'input_dir': 'downloads_mseeds',
-        'stations_json': 'station_list.json',
-        'overlap': 0.3}
+              metrics = [f1])
 
 if platform.system() == 'Windows':
     station_list = [ev.split(".")[0] for ev in listdir(args['input_dir']) if ev.split("\\")[-1] != ".DS_Store"]
@@ -62,19 +60,49 @@ for ct, st in enumerate(station_list):
 
         pred_generator = PreLoadGeneratorTest(meta["trace_start_time"], data_set, **params_pred)
 
-        #predD, predP, predS = model.predict_generator(pred_generator)
-        #pp.pprint(predD)
-        #pp.pprint(predP)
-        #pp.pprint(predS)
+        predD, predP, predS = model.predict_generator(pred_generator)
+
+# ONNX port
+print('ONNX:')
+sess_options = onnxruntime.SessionOptions()
+sess = onnxruntime.InferenceSession('eqt_model.onnx', sess_options)
+
+if platform.system() == 'Windows':
+    station_list = [ev.split(".")[0] for ev in listdir(args['input_dir']) if ev.split("\\")[-1] != ".DS_Store"]
+else:
+    station_list = [ev.split(".")[0] for ev in listdir(args['input_dir']) if ev.split("/")[-1] != ".DS_Store"]
+
+station_list = sorted(set(station_list))
+
+for ct, st in enumerate(station_list):
+    if platform.system() == 'Windows':
+        file_list = [join(st, ev) for ev in listdir(args["input_dir"]+"\\"+st) if ev.split("\\")[-1].split(".")[-1].lower() == "mseed"]
+    else:
+        file_list = [join(st, ev) for ev in listdir(args["input_dir"]+"/"+st) if ev.split("/")[-1].split(".")[-1].lower() == "mseed"]
+
+    mon = [ev.split('__')[1]+'__'+ev.split('__')[2] for ev in file_list]
+    uni_list = list(set(mon))
+    uni_list.sort()
+
+    time_slots, comp_types = [], []
+
+    for _, month in enumerate(uni_list):
+        matching = [s for s in file_list if month in s]
+        print(f'{month}')
+        meta, time_slots, comp_types, data_set = _mseed2nparry(args, matching, time_slots, comp_types, st)
+
+        pred_generator = PreLoadGeneratorTest(meta["trace_start_time"], data_set, **params_pred)
 
         out_pred_generator = iter_sequence_infinite(pred_generator)
-        x = next(out_pred_generator)
-        x_test = list(x.values())[0].astype(np.float32)
-        res = sess.run(None, input_feed={'input': x_test})
-        print(len(res))
-        """steps_done = 0
+        steps_done = 0
         steps = len(pred_generator)
+
+        # ONNX model predict_generator monkey typing
         while steps_done < steps:
             generator_output = next(out_pred_generator)
-            pp.pprint(list(generator_output.values()))
-            steps_done += 1"""
+
+            x = generator_output
+            x_test = list(x.values())[0].astype(np.float32)
+            res = sess.run(None, input_feed={'input': x_test})
+
+            steps_done += 1
