fe5a72c785da69f8282325c96c01ea79a89f508a
Cuda Chen <clh960524@gmail.com>
2021-05-03 21:07:10 +0800

    create convert2ONNX function

--- save_onnx_model.py
+++ save_onnx_model.py
@@ -3,14 +3,17 @@ from keras.models import load_model
 from EQTransformer.core.EqT_utils import f1, SeqSelfAttention, FeedForward, LayerNormalization
 import keras2onnx
 
-keras.backend.set_learning_phase(0)
+def convert2ONNX(input_model_name, output_model_name):
+    keras.backend.set_learning_phase(0)
+    model = load_model(input_model_name,
+                       custom_objects={
+                            'SeqSelfAttention': SeqSelfAttention, 
+                            'FeedForward': FeedForward,
+                            'LayerNormalization': LayerNormalization, 
+                            'f1': f1})
 
-model = load_model('EqT_model.h5',
-                   custom_objects={
-                        'SeqSelfAttention': SeqSelfAttention, 
-                        'FeedForward': FeedForward,
-                        'LayerNormalization': LayerNormalization, 
-                        'f1': f1})
+    onnx_model = keras2onnx.convert_keras(model, debug_mode=1)
+    keras2onnx.save_model(onnx_model, output_model_name)
 
-onnx_model = keras2onnx.convert_keras(model, debug_mode=1)
-keras2onnx.save_model(onnx_model, 'eqt_model.onnx')
+if __name__ == '__main__':
+    convert2ONNX('EqT_model.h5', 'eqt_model.onnx')
